e32309a30ed2eecc02c74859c995d977fbe029b3
YasasTennakoon <yasas.20200733@iit.ac.lk>
2024-04-24 07:36:55 +0530

    Initial Commit

---
+++ shoe_store/shoe_storeApp.swift
@@ -0,0 +1,17 @@
+//
+//  shoe_storeApp.swift
+//  shoe_store
+//
+//  Created by Yasas Tennakoon on 2024-04-24.
+//
+
+import SwiftUI
+
+@main
+struct shoe_storeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ shoe_storeUITests/shoe_storeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  shoe_storeUITestsLaunchTests.swift
+//  shoe_storeUITests
+//
+//  Created by Yasas Tennakoon on 2024-04-24.
+//
+
+import XCTest
+
+final class shoe_storeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
